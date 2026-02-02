0c59bff166819261fb10f123cbd889304b694f88
Sungsu Oh <sungsu.oh1203@gmail.com>
2026-02-02 20:03:41 +0900

    Initial commit: GPT Agent source code

--- refactor_code/web_rag/http.py
+++
@@ -1,8 +0,0 @@
-import os, requests, certifi
-session = requests.Session()
-session.headers.update({"User-Agent": os.getenv("USER_AGENT", "BookWriterBot/1.0")})
-session.verify = certifi.where()
-
-def http_get(url, **kw):
-    kw.setdefault("timeout", (6, 20))
-    return session.get(url, **kw)
